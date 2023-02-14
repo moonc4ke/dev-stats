f7ee397b2d616473addce054a72b2aa1133eeea6
mooncake <adom.donatas@gmail.com>
2023-02-14 17:05:12 +0200

    Add git-stats mock file and create DevView + small cleanup

---
+++ src/views/DevView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div>Profile Pic</div>
+    <div>Location</div>
+    <div>Motto</div>
+    <div>Current Level</div>
+    <div>Contributions calendar</div>
+    <div>Achievements</div>
+    <div>Main tech stack</div>
+  </div>
+</template>
+
+<style scoped></style>
